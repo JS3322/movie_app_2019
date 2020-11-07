915f2b1b7727cf4348caf1027cd45cb979d77f87
JS <i30203322@ganada.kr>
2020-11-07 17:01:35 +0900

    test20201107

--- src/routes/About.js
+++ src/routes/About.js
@@ -9,7 +9,7 @@ function About(props) {
         “Freedom is the freedom to say that two plus two make four. If that is
         granted, all else follows.”
       </span>
-      <span>− George Orwell, 1984</span>
+      <span>− George Orwell, 1984 _ test</span>
     </div>
   );
 }
